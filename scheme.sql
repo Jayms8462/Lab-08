c4f2f03d5aff51aacbc388beb4fac5488438f7ad
Jayms8462 <Jayms.Portune@gmail.com>
2019-08-11 17:20:17 -0700

    Fixing Lab 8 files

---
+++ scheme.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS locations (
+    search_query TEXT,
+    formatted_query TEXT,
+    latitude DECIMAL,
+    longitude DECIMAL
+)
--- server.js
+++ server.js
@@ -11,18 +11,14 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 // container for .env variables
-const client = new pg.Client(precess.end.DATABASE_URL);
+// const client = new pg.Client(precess.end.DATABASE_URL);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-app.get('/location', (request, response) => {
-    searchLoc(request.query.data) //pulls what was input into field
-        .then(location => {
-            response.send(location) //responds with text entered into field
-        })
-});
+app.get('/location', searchLoc);
+app.get('/weather', searchWea);
+app.get('/events', searchEve)
 
-// function constructor
 function Location(query, data) {
     this.search_query = query; //outputs what was entered into field
     this.formatted_query = data.body.results[0].formatted_address; //outputs the formatted_query in JSON AKA the City, State, Country
@@ -30,43 +26,56 @@ function Location(query, data) {
     this.longitude = data.body.results[0].geometry.location.lng; //outputs the Longitude of the element in JSON
 }
 
-function searchLoc(query) {
-    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`; // defines the URL for the JSON of Maps, passes in what was put intot he field and my API key
-    return superagent.get(url)
-        .then(res => {
-            let test = new Location(query, res);
-            console.log(url);
-            return test;
+function searchLoc(request, response) {
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
+    superagent.get(url)
+        .then(result => {
+            response.send(new Location(request.query.data, result));
         })
-}
+        .catch(error => eventError(error, response))
 
-app.get('/weather', (request, response) => {
-    try {
-        searchWea(request.query.data)
-            .then(weather => {
-                console.log("Weather: ", weather);
-                response.send(weather);
-            })
-            // const jsonData = require("./data/darksky.json");
-            // const objVal = Object.values(jsonData.daily.data);
-            // const var1 = objVal.map(data => new Weather(data));
-    } catch (error) {
-        console.log('There was an error loading the weather data')
-        response.status(500).send("Server error", error);
-    }
-})
+}
 
 function Weather(data) {
     this.forecast = data.summary;
     this.time = new Date(data.time * 1000).toString().slice(0, 15);
 }
 
-function searchWea(query) {
-    const url = 'https://api.darksky.net/forecast/${}';
-    return superagent.get(url)
-        .then(res => {
-            let test = new Weather(query, res);
-            console.log(url);
-            return test;
+function searchWea(request, response) {
+    const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+    superagent.get(url)
+        .then(result => {
+            // console.log(result.body.daily.data);
+            const wea = result.body.daily.data.map(
+                test => new Weather(test)
+            );
+            response.send(wea);
         })
+        .catch(error => eventError(error, response))
+}
+
+function Event(data) {
+    this.link = data.url;
+    this.name = data.name.text;
+    this.event_date = new Date(data.start.local).toString().slice(0, 15);
+    this.summary = data.summary;
+}
+
+function searchEve(request, response) {
+    const url = `https://www.eventbriteapi.com/v3/events/search?token=${process.env.EVENTBRITE_API_KEY}&location.address=${request.query.data.search_query}`;
+    superagent.get(url)
+        .then(result => {
+            const events = result.body.events.map(data => {
+                return new Event(data);
+            });
+            response.send(events);
+        })
+        .catch(error => eventError(error, response))
+}
+
+function eventError() {
+    console.error(error);
+    if (response) {
+        response.status(500).send("Sorry, something went wrong here.");
+    }
 }
